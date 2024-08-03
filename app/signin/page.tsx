aca69859e29d66884b8cc6ee029d2a8f88eebf40
dagim-mante <dagimawimantefardo@gmail.com>
2024-08-03 23:25:14 +0300

    added react-hook-form and zod for form validation

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,25 +1,49 @@
 'use client'
 import Link from 'next/link'
-import { useState } from 'react'
 import {useRouter} from 'next/navigation'
 import { signIn } from 'next-auth/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { z, ZodType } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type FormFields = {
+    email: string
+    password: string
+}
+
+const formSchema:ZodType<FormFields> = z
+    .object({
+        email : z
+            .string()
+            .min(1, 'Please enter an email.')
+            .email(),
+        password: z
+            .string()
+            .min(1, {message: 'Please enter a password'})
+            .min(8, {message: 'Password must be a minimum of 8 charachters'})
+    })
 
 export default function Signin(){
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: {errors, isSubmitting}
+    } = useForm<FormFields>({
+        resolver: zodResolver(formSchema)
+    })
 
     const router = useRouter()
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+    const handleSubmitRequest:SubmitHandler<FormFields> = async (data) => {
         const signinData = await signIn('credentials', {
-            email,
-            password,
+            email: data.email,
+            password: data.password,
             redirect: false
         })
 
         if(signinData?.error){
-            console.log(signinData)
+            setError('root', {message: 'Unsuccesful Login!'})
         }else{
             router.push('/')
             router.refresh()
@@ -29,28 +53,34 @@ export default function Signin(){
     return (
         <>
             <h2 className='text-xl mb-2'>Login</h2>
-            <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+            {errors.root && <div className='text-red-500'>{errors.root.message}</div>}
+            <form onSubmit={handleSubmit(handleSubmitRequest)} className="flex flex-col gap-2">
                 <div>
                     <label htmlFor='email'>Email: </label>
                     <input
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)} 
+                        {...register('email')}
                         id='email' 
                         type='text' 
-                        className="border border-slate-500 px-2 py-1 w-full"
+                        className={`border ${errors.email ? 'border-red-500': 'border-slate-500'} px-2 py-1 w-full`}
                     />
+                    {errors.email && <div className='text-red-500'>{errors.email.message}</div>}
                 </div>
                 <div>
                     <label htmlFor='password'>Password: </label>
                     <input 
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)} 
+                        {...register('password')}
                         id='password'
                         type='password' 
-                        className="border border-slate-500 px-2 py-1 w-full"
+                        className={`border ${errors.password ? 'border-red-500': 'border-slate-500'} px-2 py-1 w-full`}
                     />
+                    {errors.password && <div className='text-red-500'>{errors.password.message}</div>}
                 </div>
-                <button className="bg-green-500 px-6 py-2 w-fit text-white">Log in</button>
+                <button 
+                    disabled={isSubmitting}
+                    className={`${isSubmitting ? 'bg-gray-500' : 'bg-green-500'} px-6 py-2 w-fit text-white`}
+                >
+                    {isSubmitting ? 'Logging in...' : 'Log in'}
+                </button>
                 <span className='text-[16px] text-slate-500'>
                     Don't have an account yet?
                     <Link href='/signup' className='ml-2 text-blue-800 hover:underline'>Sign up</Link>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,79 +1,126 @@
 'use client'
 import Link from 'next/link'
-import { useState } from 'react'
 import {useRouter} from 'next/navigation'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { z, ZodType } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type Formfields = {
+    username: string
+    email: string
+    password: string
+    confirmPassword: string
+}
+
+const userSchema:ZodType<Formfields> = z
+    .object({
+        username: z
+            .string()
+            .min(1, 'Please enter a username')
+            .min(4, 'Username needs minimum of 4 charchters.')
+            .max(10, 'Username must have maximum of 10 charchters.'),
+        email : z
+            .string()
+            .min(1, 'Please enter an email address.')
+            .email(),
+        password: z
+            .string()
+            .min(1, 'Please enter a password')
+            .min(8, 'Password needs minimum of 8 charchters.')
+            .max(20, 'Password must have maximum of 20 charchters.'),
+        confirmPassword: z
+            .string()
+            .min(1, 'Please confirm your password.')
+    }).refine(data => data.password === data.confirmPassword , {
+        message: 'Passwords don\'t match.',
+        path: ['confirmPassword']
+    })
 
 export default function Signup(){
-    const [username, setUsername] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: {errors, isSubmitting}
+    } = useForm<Formfields>({
+        resolver: zodResolver(userSchema)
+    })
 
     const router = useRouter()
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+    const handleSubmitHandler:SubmitHandler<Formfields> = async (data) => {
         const res = await fetch('http://localhost:3000/api/user', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({username, email, password, confirmPassword})
+            body: JSON.stringify({
+                username: data.username,
+                email: data.email,
+                password: data.password,
+                confirmPassword: data.confirmPassword
+            })            
         })
 
         if(res.ok){
             router.push('/signin')
         }else{
             const {message} = await res.json()
-            console.log(message)
+            setError('root', {message})
         }
     } 
 
     return (
         <>
             <h2 className='text-xl mb-2'>Signup</h2>
-            <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+            {errors.root && <div className='text-red-500'>{errors.root.message}</div>}
+            <form onSubmit={handleSubmit(handleSubmitHandler)} className="flex flex-col gap-2">
                 <div>
                     <label htmlFor='username'>Username: </label>
                     <input
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)} 
+                        {...register('username')}
                         id='username'
                         type='text'
                         className="border border-slate-500 px-2 py-1 w-full"
                     />
+                    {errors.username && <div className='text-red-500'>{errors.username.message}</div>}
                 </div>
                 <div>
                     <label htmlFor='email'>Email: </label>
                     <input
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)} 
+                        {...register('email')}
                         id='email' 
                         type='text' 
                         className="border border-slate-500 px-2 py-1 w-full"
                     />
+                    {errors.email && <div className='text-red-500'>{errors.email.message}</div>}
                 </div>
                 <div>
                     <label htmlFor='password'>Password: </label>
                     <input 
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)} 
+                        {...register('password')}
                         id='password'
                         type='password' 
                         className="border border-slate-500 px-2 py-1 w-full"
                     />
+                    {errors.password && <div className='text-red-500'>{errors.password.message}</div>}
                 </div>
                 <div>
                     <label htmlFor='confirmPassword'>Confirm Password: </label>
                     <input
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)} 
+                        {...register('confirmPassword')}
                         id='confirmPassword' 
                         type='password' 
                         className="border border-slate-500 px-2 py-1 w-full"
                     />
+                    {errors.confirmPassword && <div className='text-red-500'>{errors.confirmPassword.message}</div>}
                 </div>
-                <button className="bg-green-500 px-6 py-2 w-fit text-white">Sign up</button>
+                <button
+                    disabled={isSubmitting}
+                    className={`${isSubmitting ? 'bg-gray-500': 'bg-green-500'} px-6 py-2 w-fit text-white`}
+                >
+                    {isSubmitting ? 'Signing up...' : 'Sign up'}
+                </button>
                 <span className='text-[16px] text-slate-500'>
                     Already have an account?
                     <Link href='/signin' className='ml-2 text-blue-800 hover:underline'>Sign in</Link>
