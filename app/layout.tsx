944d699d1a8778340ee3adebe7f735542a645829
dagim-mante <dagimawimantefardo@gmail.com>
2024-10-22 15:13:38 +0300

    open graph image 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,16 +22,6 @@ export default async function RootLayout({
   const session = await auth()
   return (
     <html lang="en">
-    <head>
-        <meta property="og:image" content="<generated>" />
-        <meta property="og:image:type" content="<generated>" />
-        <meta property="og:image:width" content="<generated>" />
-        <meta property="og:image:height" content="<generated>" />
-<meta name="twitter:image" content="<generated>" />
-<meta name="twitter:image:type" content="<generated>" />
-<meta name="twitter:image:width" content="<generated>" />
-<meta name="twitter:image:height" content="<generated>" />
-        </head>
       <body className={inter.className}>
         <div className="flex-grow px-6 md:px-12 mx-auto max-w-7xl">
           <NavBar session={session}/>
